c8ba5a0bf6e1dc1f53fadcca911b319209287dec
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-02-24 15:29:48 -0300

    feat: conserta a conexão com o bd

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,16 @@
 import db from "./database/database.js";
 import app from "./app.js";
 
-db.authenticate()
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  db.authenticate()
   .then(() => {
-    const PORT = process.env.PORT || 3000;
+  
     db.sync();
     console.log("Database connected.");
-    app.listen(PORT, () => {
-      console.log(`App is running on http://localhost:${PORT}`);
-    });
+    console.log(`Server running on port ${PORT}`);
   })
   .catch((error) => {
     console.error("Unable to connect to the database:", error);
   });
+})
