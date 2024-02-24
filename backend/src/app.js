1fe6b5bd6936474ef15df05c942d89f2a0de71c6
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-02-24 17:27:47 -0300

    feat: commit inicial do frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,16 +5,12 @@ import "express-async-errors";
 import userRoutes from "./routes/user.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import { handleErrors } from "./middlewares/handleErrors.middlewares.js";
-
 const app = express();
 
 app.use(helmet())
 app.use(express.json());
 app.use(cors());
 
-app.use("/doc", (req, res) => {
-    res.send("Documentação da API Lexart Labs.");
-})
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use(handleErrors);
