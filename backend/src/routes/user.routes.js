28e9d91573f376bc5ffb18b2923d5c86cca21658
Julia Pereira Teixeira <juliapt95@gmail.com>
2024-02-26 15:26:11 -0300

    Update user.routes.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -7,9 +7,9 @@ const userController = new UserController();
 const userMiddlewares = new UserMiddlewares();
 userRoutes.post("", userController.create);
 userRoutes.post("/login", userController.login);
-
+userRoutes.get("/:id", userMiddlewares.isTokenValid,userMiddlewares.userIdParams, userController.getById)
+userRoutes.get("",  userMiddlewares.isTokenValid,userMiddlewares.userIdParams, userController.getById)
 userRoutes.use("/:id", userMiddlewares.isTokenValid,userMiddlewares.userIdParams, userMiddlewares.isUserOwner)
-userRoutes.get("/:id", userController.getById)
 userRoutes.patch("/:id", userController.update);
 userRoutes.delete("/:id", userController.delete);
 
